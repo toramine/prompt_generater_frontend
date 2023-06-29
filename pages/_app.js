3e66dbd52fc8a29f9112292f821971482c7cd454
toramine <a>
2023-06-29 15:48:44 +0900

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import '../styles/globals.css'
+import TopBar from '../components/TopBar'
+import onDragEnd from '../components/GeneratePrompt'
+import GeneratePrompt from '../components/GeneratePrompt'
+import { DragDropContext } from "react-beautiful-dnd";
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <DragDropContext onDragEnd={onDragEnd}> */}
+        <TopBar />
+        <Component {...pageProps} />
+      {/* </DragDropContext> */}
+    </>
+  )
 }
